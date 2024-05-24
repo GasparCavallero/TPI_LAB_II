16c1443f34eaf83b91a38369ba683552a65d452e
Gaspar Cavallero <gaspicava1510@gmail.com>
2024-05-24 17:55:07 -0300

    Parte controlador raza y propietario

--- Controladores/Controlador_Propietario.py
+++ Controladores/Controlador_Propietario.py
@@ -1,8 +1,21 @@
-
 from Models.Propietario import Propietario
 from Vistas.Vista_Propietario import Vista_Propietario
 
+
 class Controlador_Propietario:
     def __init__(self):
-        self.vista = Vista_Propietario()
-        self.modelo = Propietario()
+        self._vista = Vista_Propietario()
+        self._modelo = Propietario()
+
+    def Buscar_Propietario(self, codigo):
+        with open("propietario.txt", "r") as file:
+            for line in file:
+                linea = line.split(",")
+                if int(linea[0]) == int(codigo):
+                    self._modelo = Propietario(linea[0], linea[1])
+                    self._modelo.setDatos_Propietario([])
+                    return self._modelo
+        return None
+
+    def Mostrar_Datos(self):
+        self._vista.Visualizar_Datos(self._modelo.getDatos_Propietario())
--- Controladores/Controlador_Raza.py
+++ Controladores/Controlador_Raza.py
@@ -1,8 +1,21 @@
-
 from Models.Raza import Raza
 from Vistas.Vista_Raza import Vista_Raza
 
+
 class Controlador_Raza:
     def __init__(self):
-        self.vista = Vista_Raza()
-        self.modelo = Raza()
+        self._vista = Vista_Raza()
+        self._modelo = Raza()
+
+    def Buscar_Raza(self, codigo):
+        with open("raza.txt", "r") as file:
+            for line in file:
+                linea = line.split(",")
+                if int(linea[0]) == int(codigo):
+                    self._modelo = Raza(linea[0], linea[1])
+                    self._modelo.setDatos_Raza([])
+                    return self._modelo
+        return None
+
+    def Mostrar_Datos(self):
+        self._vista.Visualizar_Datos(self._modelo.getDatos_Raza())
