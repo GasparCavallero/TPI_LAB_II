0ea67f158982994de6e1d2044e8a7d3807915414
unknown <santiagovirovoy@gmail.com>
2024-05-28 00:45:48 -0300

    modificacion de controladores

--- Controladores/ControladorConsulta.py
+++ Controladores/ControladorConsulta.py
@@ -5,4 +5,4 @@ from Vistas.Vista_Consulta import Vista_Consulta
 class Controlador_Consulta:
     def __init__(self):
         self.vista = Vista_Consulta()
-        self.modelo = Consulta()
+        self.modelo = Consulta() 
--- Controladores/ControladorVeterinario.py
+++ Controladores/ControladorVeterinario.py
@@ -6,3 +6,10 @@ class Controlador_Veterinario:
     def __init__(self):
         self.vista = Vista_Veterinario()
         self.modelo = Veterinario()
+
+    def cargar_veterinario(self):
+        with open("\\Archivos\\socios.txt", "r") as file:
+            lineas = file.readlines()
+        for i in lineas:
+            especialidad, nombre, apellido, fechaNac, correo = i.strip().split(",")
+            self._listaSocios.append(Veterinario(especialidad, nombre, apellido, fechaNac, correo))
